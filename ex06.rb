27576431886936e3de7441e9e4063bf18e3f1213
Alex <alex.satur89@gmail.com>
2017-04-12 16:36:11 +0100

    Ex06

---
+++ ex06.rb
@@ -0,0 +1,28 @@
+types_of_people = 10 # sets varialbe type of people equal to 10
+x = "There are #{types_of_people} types of people." #sets varialbe x equal to a string with a variable
+binary = "binary" #sets binary equal to the string binary
+do_not = "don't" 
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x #puts x to the console
+puts y
+
+puts "I said #{x}." #puts string plus variable 
+puts "I also said: '#{y}'."
+
+hilarious = false #sets hilarious to false
+joke_evaluation = "Isnt that joke so funny?! #{hilarious}" #puts joke evluation to a string
+
+puts joke_evaluation 
+
+w = "this is the left side of..."
+e = "a string with a right side."
+
+puts w + e
+
+=begin
+Study Drills
+
+2. string inside a string, lines 5 twice and lines 10 and 11
+4. because they are both strings
+5. no because you would need to come out and use a plus symbol
