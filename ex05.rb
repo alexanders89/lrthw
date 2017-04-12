2fa6ee86cae1f8270d2d125dd2f36ed597f983c3
Alex <alex.satur89@gmail.com>
2017-04-12 16:12:14 +0100

    Ex05

---
+++ ex05.rb
@@ -0,0 +1,39 @@
+my_name = 'Zed A. Shaw'
+my_age = 35
+my_height = 74 #inches
+my_weight = 180 #lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Lets talk about #{my_name}"
+puts "He's #{my_height} inches tall"
+puts "He's #{my_weight}lbs heavy"
+puts "Actually thats not too heavy"
+
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee"
+
+puts "If i add #{my_age}, #{my_height}, and #{my_weight}
+      I get #{my_age + my_height + my_weight}."
+
+#Study Drills
+
+name = 'Zed A. Shaw'
+age = 35
+height = 74 #inches
+weight = 180 #lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Lets talk about #{name}"
+puts "He's #{height} inches tall, which is #{height * 2.5}cm tall"
+puts "He's #{weight}lbs heavy, which is #{weight * 0.453592}kg"
+puts "Actually thats not too heavy"
+
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee"
+
+puts "If i add #{age}, #{height}, and #{weight}
+      I get #{age + height + weight}."
