a2d1564269e738d3b5c02433fdac1fa875cde67d
Alex <alex.satur89@gmail.com>
2017-04-12 14:46:11 +0100

    Ex 02

---
+++ ex02.rb
@@ -0,0 +1,11 @@
+# A comment, this is so you can read your programs later.
+# Anything after the # is ignored by ruby
+
+puts "I could have code like this." #and the comment after is ignored
+
+#you can also use comment to disable or comment out a piece of code:
+#puts "this wont run"
+puts "this will run! :)"
+
+#study drills
+
