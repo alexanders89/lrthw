468bc2f667af7129bc3d3782b317fcf2490b12b7
Alex <alex.satur89@gmail.com>
2017-04-12 15:24:41 +0100

    Ex03

---
+++ ex03.rb
@@ -0,0 +1,49 @@
+puts "i will now count my chickens:" #puts a string
+
+puts "Hens #{25 + 30 / 6}"
+puts "roosters #{100 - 25 * 3 % 4}"
+
+puts "Now i will count the eggs!:"
+
+puts 3 + 2 + 1 -5 + 4 % 2 - 1 / 4 + 6
+
+puts "is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "what is 5 - 7? #{ 5 - 7}"
+
+puts "Oh thats why its false."
+
+puts "how about some more."
+
+puts "Is it greater? #{ 5 > -2}"
+puts "is it greater or equal #{5 >= -2}"
+puts "is it less or equal? #{5 <= -2}"
+
+#Floating Point Numbers
+
+puts "i will now count my chickens:" #puts a string
+
+puts "Hens #{25.0 + 30.0 / 6.0}"
+puts "roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+puts "Now i will count the eggs!:"
+
+puts 3.0 + 2.0 + 1.0 -5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+puts "is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+puts "What is 3.0 + 2.0? #{3.0 + 2.0}"
+puts "what is 5.0 - 7.0? #{ 5.0 - 7.0}"
+
+puts "Oh thats why its false."
+
+puts "how about some more."
+
+puts "Is it greater? #{ 5.0 > -2.0}"
+puts "is it greater or equal #{5.0 >= -2.0}"
+puts "is it less or equal? #{5.0 <= -2.0}"
