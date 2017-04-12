52cd74dc65be48ced5b6e92fc7d275d84c4da0d7
Alex <alex.satur89@gmail.com>
2017-04-12 14:40:21 +0100

    Ex 1

--- ex01.rb
+++ ex01.rb
@@ -5,3 +5,11 @@ puts "this is fun!"
 puts "yay printing!"
 puts "I'd much rather you 'not'."
 puts 'I "said" do not touch this'
+
+#study drills
+
+#01.
+puts ""
+
+#02.
+# turns it into a comment and is ignored by ruby
