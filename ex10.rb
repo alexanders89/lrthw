68e58d682877aa1f06c526955d01d4a3deb6aab7
Alex <alex.satur89@gmail.com>
2017-04-12 17:28:44 +0100

    Ex10

---
+++ ex10.rb
@@ -0,0 +1,34 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \ \ a \ \ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
+=begin
+
+Study Drills
+
+\\ = Backslash()
+\' = Single Quote (')
+\" = Double Quote (")
+\a = ASCII bell (BEL)
+\b = ASCII backspace (BS)
+\f = ACSII formfeed (FF)
+\n = ASCII linefeed (LF)
+\r ASCII = Carriage Return (CR)
+\t ASCII = Horizontal Tab (TAB)
+\uxxxx = Character with 16 bit hex value xxxx (Unicode only)
+\Uxxxxxxxx = Character with 32bit hex value xxxxxxxx (Unicode only)
+\v = ASCII Vertical tab (VT)
+\ooo = Character woth octal value ooo
+\xhh = Character with hex value hh
