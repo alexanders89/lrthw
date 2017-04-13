9a31ab8baff52e76eb9854cc37320e431e38ddd0
Alex <alex.satur89@gmail.com>
2017-04-13 15:36:23 +0100

    Ex11 and 12

---
+++ ex11_ex12.rb
@@ -0,0 +1,27 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+#Study Drills
+
+#1. gets gets input from the user, chomp takes the return off the answer
+
+#ex 12
+
+print "give me a number: "
+number = gets.chomp.to_i
+
+bigger = number * 100
+puts "A bigger number is #{bigger}."
+
+print "Give me another number: "
+another = gets.chomp
+number = another.to_i
+
+smaller = number / 100
+puts "A smaller number is #{smaller}"
