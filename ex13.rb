b31ce410bc47f1c0de09134a4c9b32ae3497dfba
Alex <alex.satur89@gmail.com>
2017-04-13 16:25:02 +0100

    Ex13

--- ex13.rb
+++ ex13.rb
@@ -1,6 +1,7 @@
-first, second, third, fourth = ARGV
+a, b, c, d, e = ARGV
 
-puts "Your first variable is: #{first}"
-puts "Your second variable is #{second}"
-puts "Your third variable is #{third}"
-puts "Your fourth variable is #{fourth}"
+puts "Your first variable is: #{a}"
+puts "Your second variable is #{b}"
+puts "Your third variable is #{c}"
+puts "Your fourth variable is #{d}"
+puts "Your fourth variable is #{e}"
